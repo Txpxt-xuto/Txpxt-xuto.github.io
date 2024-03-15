bd9d439abf56fbc469a496c2d5d68e3355235a31
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-15 15:53:59 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 console.log("hello")
 console.log("hello")
-console.log("hello")
-console.log("hello")
+console.log("Tapat Toungsakul")
+console.log("My website")
+let x=prompt('Enter')
+if(x>=50)
+{
+    console.log("pass")
+}
+else { console.log("Fail")}
