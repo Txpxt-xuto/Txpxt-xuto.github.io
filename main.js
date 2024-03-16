17fa1228b820212feab5535a33a326a2fb4763ce
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-16 19:23:33 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 console.log("Hello")
 console.log("Tapat Toungsakul")
 console.log("My website")
-let x=prompt('Enter')
 console.log("ธภัทร ตึ้งสกุล")
 console.log("ออโต้")
 console.log("0982625255")
