41752418f1f7a47ec6d12c4c1de06c6a02b5631a
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-19 21:28:58 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,3 +7,8 @@ console.log("0982625255")
 console.log("Au To")
 console.log("txpxt_xuto")
 console.log("txpxt-xuto")
+
+const scroller = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+});
