51df67f95f4cf1fbbdb1ba3d837b881acdf401f6
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-20 19:59:52 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
+let x = 10;
+var name = "Tapat toungsakul";
+const pi = 3.14159265;
+
 console.log("Hello")
 console.log("Tapat Toungsakul")
 console.log("My website")
 console.log("ธภัทร ตึ้งสกุล")
 console.log("ออโต้")
+
+function add(a, b)
+{
+    return a+b;
+}
+console.log(add(3,5));
+for(let i = 0;i<5;i++)
+{
+    console.log(add(11,9));
+}
+
+let arr =[1,2,3,4,5];
+console.log(arr[4]);
+arr.push(1);
+console.log(arr);
+
+
 console.log("0982625255")
 console.log("Au To")
 console.log("txpxt_xuto")
 console.log("txpxt-xuto")
-
-const scroller = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true
-});
