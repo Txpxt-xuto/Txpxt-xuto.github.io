c73c577118713bb043e61f2aa20215313b22a4cd
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-31 19:01:48 +0700

    Update main.js

--- main.js
+++ main.js
@@ -28,3 +28,18 @@ console.log("0982625255")
 console.log("Au To")
 console.log("txpxt_xuto")
 console.log("txpxt-xuto")
+const cursorSmall = document.querySelector('.small');
+const cursorBig = document.querySelector('.big');
+
+
+const positionElement = (e)=> {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+   
+  cursorSmall.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  
+  cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+ 
+}
+
+window.addEventListener('mousemove', positionElement)
