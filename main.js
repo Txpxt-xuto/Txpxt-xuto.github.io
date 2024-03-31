6a449c3710fb16776f628aa6ab07c45cf9890927
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-31 21:11:56 +0700

    Update main.js

--- main.js
+++ main.js
@@ -32,14 +32,14 @@ const cursorSmall = document.querySelector('.small');
 const cursorBig = document.querySelector('.big');
 
 
-const positionElement = (e)=> {
+const positionElement = (e)=>
+{
   const mouseY = e.clientY;
   const mouseX = e.clientX;
-   
+  
   cursorSmall.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
   
   cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
- 
 }
 
 window.addEventListener('mousemove', positionElement)
