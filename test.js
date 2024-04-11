499eec2d10133c0d52cbe1794ff02699487ee60e
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-04-11 20:46:05 +0700

    Update test.js

--- test.js
+++ test.js
@@ -149,27 +149,32 @@ themeButton.addEventListener('click', () => {
 })*/ 
 const lightIcon = document.getElementById("light-icon");
 const darkIcon = document.getElementById("dark-icon");
-
 const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
 let darkMode = darkModeMediaQuery.matches;
-
 // Set dark-mode class on body if darkMode is true and pick icon
-if (darkMode) {
+if (darkMode) 
+{
     document.body.classList.add("dark-mode");
     darkIcon.setAttribute("display", "none");
-} else {
+} 
+else 
+{
     lightIcon.setAttribute("display", "none");
 }
-function toggleDarkMode() {
+function toggleDarkMode() 
+{
     // Toggle darkMode variable
     darkMode = !darkMode;
     // Toggle dark-mode class on body
-    document.body.classList.toggle("dark-mode");
+    document.body.classList.toggle('dark-theme');
     // Toggle light and dark icons
-    if (darkMode) {
+    if (darkMode)
+    {
         lightIcon.setAttribute("display", "block");
         darkIcon.setAttribute("display", "none");
-    } else {
+    } 
+    else 
+    {
         lightIcon.setAttribute("display", "none");
         darkIcon.setAttribute("display", "block");
     }
